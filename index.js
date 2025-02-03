c332dd0513dd8841dd65bf80ce56033cfeb1c969
gokulgp08 <gokulmanoharan053@gmail.com>
2025-02-03 15:52:03 +0530

    fix

---
+++ index.js
@@ -0,0 +1,97 @@
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const number = document.getElementById('number');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const confirmpassword = document.getElementById('confirmpassword');
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+const validateInputs = () => {
+
+    const firstnameValue = firstname.value.trim();
+    const lastnameValue = lastname.value.trim();
+    const emailValue = email.value.trim();
+    const numberValue = number.value.trim();
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+    const confirmpasswordValue = confirmpassword.value.trim();
+
+    if(firstnameValue === '') {
+        setError(firstname, 'First name is required');
+    } else {
+        setSuccess(firstname);
+    }
+
+    if(lastnameValue === '') {
+        setError(lastname, 'Last namre is required');
+    } else {
+        setSuccess(lastname);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email');
+    } else {
+        setSuccess(email);
+    }
+
+    if(numberValue === '') {
+        setError(number, 'Phone number is required');
+    } else {
+        setSuccess(number);
+    }
+
+    if(usernameValue === '') {
+        setError(username, 'User namre is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'password is required');
+    }else if (passwordValue.length < 6) {
+        setError(password, 'password must be 6 character');
+    } else {
+        setSuccess(password);
+    }
+
+    if(confirmpasswordValue === '') {
+        setError(confirmpassword, 'please confirm your password');
+    }else if (passwordValue !== confirmpasswordValue) {
+        setError(confirmpassword, 'password dosent match');
+    }else {
+        setSuccess(confirmpassword);
+    }
+
+};
--- user-register.php
+++ user-register.php
@@ -10,39 +10,39 @@
         
 
 
-    if($_SERVER ["REQUEST_METHOD"] == "POST"){
+    // if($_SERVER ["REQUEST_METHOD"] == "POST"){
 
-        $first_name = trim($_POST['first_name']);
+    //     $first_name = trim($_POST['first_name']);
 
-        $last_name = trim($_POST['last_name']);
+    //     $last_name = trim($_POST['last_name']);
 
-        $email = trim($_POST['email']);
+    //     $email = trim($_POST['email']);
 
-        $phone = trim($_POST['phone']);
+    //     $phone = trim($_POST['phone']);
 
-        $user_name = trim($_POST['user_name']);
+    //     $user_name = trim($_POST['user_name']);
 
-        $password = trim($_POST['password']);
+    //     $password = trim($_POST['password']);
 
-        $confirm_password = trim($_POST['confirm_password']);
-
-
+    //     $confirm_password = trim($_POST['confirm_password']);
 
 
-        $sql = "INSERT INTO users (first_name, last_name, email, phone_number, user_name, password) 
-                VALUES ('$first_name', '$last_name', '$email', '$phone', '$user_name', '$password')";
 
-        if (mysqli_query( $con, $sql)) {
 
-            echo "User registered successfully";
+    //     $sql = "INSERT INTO users (first_name, last_name, email, phone_number, user_name, password) 
+    //             VALUES ('$first_name', '$last_name', '$email', '$phone', '$user_name', '$password')";
 
-        } else {
+    //     if (mysqli_query( $con, $sql)) {
 
-            echo "Error registering user". mysqli_error($con);
-        }
+    //         echo "User registered successfully";
 
-        mysqli_close( $con );
-    }
+    //     } else {
+
+    //         echo "Error registering user". mysqli_error($con);
+    //     }
+
+    //     mysqli_close( $con );
+    // }
     
 
 ?>
@@ -63,44 +63,44 @@
         <form action="" method="POST" id="form">
             <div class="form-group">
                 <label for="firstname">First Name:</label>
-                <input type="text" value="<?=$first_name?>" name="first_name">
-                <div class="error"><</div>
+                <input type="text" id="firstname" name="first_name">
+                <div class="error"></div>
             </div>
 
             <div class="form-group">
                 <label for="lastname">Last Name:</label>
-                <input type="text" value="<?=$last_name?>" name="last_name">
-                <div class="error"><</div>
+                <input type="text" id="lastname" name="last_name">
+                <div class="error"></div>
             </div>
 
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" value="<?=$email?>" name="email">
-                <div class="error"><</div>
+                <input type="email" id="email" name="email">
+                <div class="error"></div>
             </div>
 
             <div class="form-group">
                 <label for="phone">Phone Number:</label>
-                <input type="tel" value="<?=$phone?>" name="phone" pattern="[0-9]{10}">
-                <div class="error"><</div>
+                <input type="tel" id="number" name="phone" pattern="[0-9]{10}">
+                <div class="error"></div>
             </div>
 
             <div class="form-group">
                 <label for="username">User name:</label>
-                <input type="text" value="<?=$user_name?>" name="user_name">
-                <div class="error"><</div>
+                <input type="text" id="username" name="user_name">
+                <div class="error"></div>
             </div>
 
             <div class="form-group">
                 <label for="password">Password:</label>
-                <input type="password" value="<?=$password?>" name="password">
-                <div class="error"><</div>
+                <input type="password" id="password" name="password">
+                <div class="error"></div>
             </div>
 
             <div class="form-group">
                 <label for="confirm_password">Confirm Password:</label>
-                <input type="password" value="<?=$confirm_password?>" name="confirm_password">
-                <div class="error"><</div>
+                <input type="password" id="confirmpassword" name="confirm_password">
+                <div class="error"></div>
             </div>
 
             <button type="submit" class="submit-btn">Register</button>
